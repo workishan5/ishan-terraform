b06fe072b816c8d44814e85247fe4b4089f65e64
workishan5 <105703278+workishan5@users.noreply.github.com>
2022-05-27 16:06:03 +0530

    Update main.tf

--- Azure Devops Pipline Terraform/main.tf
+++ Azure Devops Pipline Terraform/main.tf
@@ -1,3 +1,19 @@
+terraform {
+
+  required_version = ">=0.12"
+  
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
 resource "azurerm_resource_group" "rg" {
   name      = "rg-for-azurepipline"
   location  = "westeurope"
